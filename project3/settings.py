28889e3dfc8326c5128097e8db21b9ee9f212a0a
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2020-10-30 08:06:39 +0500

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = 'mail.User'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+                 
+                 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
