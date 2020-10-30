080d6c53dc539694dd55ce6fb6219a81671ca4f1
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2020-10-30 08:25:40 +0500

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -124,12 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATIC_URL = '/static/'
 
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 
